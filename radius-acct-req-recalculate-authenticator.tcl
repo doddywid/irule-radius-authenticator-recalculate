1e183dd8fab667009ed02e0238377ab275044ac2
doddywid <36068721+doddywid@users.noreply.github.com>
2023-09-02 11:44:40 +0700

    Create radius-acct-req-recalculate-authenticator.tcl

---
+++ radius-acct-req-recalculate-authenticator.tcl
@@ -0,0 +1,19 @@
+############################################################################################################
+# This block should be put to the end of existing radius irules, for calculating new request authenticator #
+############################################################################################################
+    
+    #set required var to calculate new request authenticator
+    set secret "telkomsel"
+    set payload [UDP::payload]
+    
+    #calculate new request authenticator and update UDP payload
+    set secret [binary format a* $secret]
+    binary scan $secret H* secret
+    binary scan $payload H2H2H4x16H* code id len avps
+    set req_auth [binary format H* ${code}${id}${len}00000000000000000000000000000000${avps}${secret}]
+    set req_auth [md5 $req_auth]
+    binary scan $req_auth H* req_auth
+    binary scan $payload H8x16H* leftpayload rightpayload
+    set newpayload [binary format H* $leftpayload$req_auth$rightpayload]
+    UDP::payload replace 0 0 $newpayload
+    UDP::payload replace [string length $newpayload] [expr [UDP::payload length] - [string length $newpayload]] ""
